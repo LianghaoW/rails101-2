f44eb7cabaaa38ce30a508e10aca3b8e43ea77bf
lianghaowei <372024070@qq.com>
2016-12-18 15:38:41 +0800

    implemment groups#destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -14,7 +14,7 @@ class GroupsController < ApplicationController
   def edit
     @group = Group.find(params[:id])
  end
- 
+
   def create
     @group =Group.new(group_params)
     @group.save
@@ -22,7 +22,19 @@ class GroupsController < ApplicationController
       redirect_to groups_path
 end
 
+  def update
+    @group = Group.find(params[:id])
 
+    @group.update(group_params)
+    redirect_to groups_path,notice: "Update Success"
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    flash[:alert] = "Group deleted"
+    redirect_to groups_path
+  end
 
 private
 
